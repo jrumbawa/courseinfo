5afd6f4d3eaac8f6f35354e717948e46461633dd
= <rumbawajomar@gmail.com>
2022-04-26 22:32:24 -0700

    import Part component and map out parts prop

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,14 @@
 import Part from './Part'
 
-const Content = () => {
-  return <div>Content</div>
+const Content = ({ parts }) => {
+  console.log(parts)
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  )
 }
 
 export default Content
